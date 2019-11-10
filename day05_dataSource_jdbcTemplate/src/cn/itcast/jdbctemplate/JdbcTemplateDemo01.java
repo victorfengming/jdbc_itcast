f78587f78374dccfc69e4d4b524f0451a5ce4b74
victor <fengming19981221@163.com>
2019-11-10 16:30:46 +0800

    加入了单元注解,import org.junit.jupiter.api.Test能够进行单独单元测试

--- day05_dataSource_jdbcTemplate/src/cn/itcast/jdbctemplate/JdbcTemplateDemo01.java
+++ day05_dataSource_jdbcTemplate/src/cn/itcast/jdbctemplate/JdbcTemplateDemo01.java
@@ -1,5 +1,8 @@
 package cn.itcast.jdbctemplate;
 
+import cn.itcast.utils.JdbcUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * @author victor
  * @site https://victorfengming.github.io/
@@ -12,5 +15,14 @@ package cn.itcast.jdbctemplate;
 public class JdbcTemplateDemo01 {
     public static void main(String[] args) {
         //1.导入jar包
+        // 2.创建JDBCTemplate对象
+        JdbcTemplate template = new JdbcTemplate(JdbcUtils.getDataSource());
+        //3.嗲用方法
+        // 定义一个sql
+        String sql = "update account set balance = 9999 where id = ?";
+
+        int count = template.update(sql,3);
+        System.out.println(count);
+        // 这里不用释放资源了,得劲
     }
 }
---
+++ day05_dataSource_jdbcTemplate/src/cn/itcast/jdbctemplate/JdbcTemplateDemo02.java
@@ -0,0 +1,32 @@
+package cn.itcast.jdbctemplate;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author victor
+ * @site https://victorfengming.github.io/
+ * @company XDL
+ * @project itcast
+ * @package cn.itcast.jdbctemplate
+ * @created 2019-11-10 16:22
+ * @function ""
+ */
+public class JdbcTemplateDemo02 {
+    // Junit单元测试,可以方法独立执行
+
+    /**
+     * 1. 修改1号数据
+     */
+    @Test
+    public void test1() {
+        System.out.println("我被执行了");
+    }
+    /**
+     * 1. 修改1号数据
+     */
+    @Test
+    public void test2() {
+        System.out.println("我又被执行了");
+    }
+
+}
